2dd9d2ff2d3117e0d5a3c28b9613de0c96b585be
Azi <azicrawford@gmail.com>
2026-02-17 20:10:35 +0000

    made some changes to test things

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,21 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('double/:input')
+  getDouble(@Param() params): number {
+    return this.appService.getDouble(params.input);
+  }
+
+  @Get('fannify/:num')
+  getFannify(@Param() params): number {
+    return this.appService.getFannify(params.num);
+  }
+
+  @Get('dubbafan/:nummy')
+  getDubbaFan(@Param() params): number {
+    return this.appService.getDouble(this.appService.getFannify(params.nummy));
+  }
+
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,17 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  fan = 5.6;
+
+  getDouble(num: number): number { 
+    return 2*num;
+  }
+
+
+  getFannify(num: number): number {
+    return this.fan * num;
+  }
+
+
 }
